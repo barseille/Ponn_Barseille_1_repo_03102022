a9dedaae3278cd203b2928b1eedcba1ece4f0dc2
BarseillePonn <ponn.barseille@gmail.com>
2022-10-14 09:34:14 +0200

    nettoyage commentaires

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
-import csv
 import os
 from pathlib import Path
 from slugify import slugify
@@ -27,26 +26,32 @@ def bs(url):
     response = requests.get(url)
     if response.ok:
         return (BeautifulSoup(response.content, "html.parser"))
+    
 
 
 # Extraire les pages de chaques categories
 def get_category(url_principal):
     # Récupération des liens des catégories depuis la page principale
     all_category = bs(url_principal).select("ul")[2].select("li")
+    
     category_list = []
 
     # Récupération des pages index.html des catégories puis des pages suivantes
-    for i in all_category:
-        category_url = urljoin(url_principal, i.a["href"])
+    for elt in all_category:
+        category_url = urljoin(url_principal, elt.a["href"])
 
         category_list.append(category_url)
         button_next = bs(category_url).find("li", class_="next")
+       
+       
 
         # tant qu'il existe un bouton "next", on récupère les pages catégories suivantes
         while button_next:
             next_page = urljoin(category_url, button_next.a["href"])
-
+        
+        
             button_next = bs(next_page).find("li", class_="next")
+          
             category_list.append(next_page)
 
     return (category_list)
@@ -61,6 +66,7 @@ def get_books(category_url):
     for i in category_soup:
         book_url = urljoin(category_url, i.a["href"])
         book_list.append(book_url)
+        
 
     return (book_list)
 
@@ -71,7 +77,7 @@ def get_data(soup_book):
     product_page_url = book_url
     universal_product_code = tds[0].text
     title = soup_book.h1.text
-    title = slugify(title, lowercase=False, separator=" ")
+    title = slugify(title,separator=" ")
     price_including_tax = tds[3].text
     price_excluding_tax = tds[2].text
     number_available = tds[5].text
@@ -96,10 +102,12 @@ def get_data(soup_book):
              product_description,
              categorie_name,
              review_rating,
-             image_url])
+             image_url
+             ])
 
 def create_csv_file(categorie_name, valeurs):
-    # Créer un chemin concret avec Path en mode append et je joins un point-virgule entre les valeurs
+    
+    # Créer un chemin concret avec Path en mode "append" et je sépare un point-virgule entre les valeurs
     with open(Path(path, f"{categorie_name}.csv"), "a", encoding="UTF8") as file:
         return (file.write(";".join(valeurs) + "\n"))
 
@@ -108,13 +116,11 @@ for category_url in get_category(url_principal):
 
     # récupération de tous les URL de catégories
     category_soup = bs(category_url)
-    print(category_soup)
-
+   
     # Recherche du nom de la catégorie
     categorie_name = category_soup.find("h1").text
     path = categorie_name
-    print(path)
-
+    
     # Création du répertoire avec les noms des catégories
     if not os.path.exists(categorie_name):
         os.mkdir(categorie_name)
@@ -135,5 +141,5 @@ for category_url in get_category(url_principal):
 
         # valeur[2] = title
         # "wb" mode écriture binaire
-        with open(Path(path, f"{valeurs[2]}.jpg"), "wb") as file:
+        with open(Path(path, f"{valeurs[2][:50]}.jpg"), "wb") as file:
             file.write(image)
